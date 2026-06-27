5ccfa7e9e64415ac3f6d1a2d7639d904a5c8e362
MihaelaGavrilova04 <mihaela_gavrilo@abv.bg>
2026-06-27 20:08:59 +0300

    Fix code to follow the checkstyle ruleset

--- server/src/main/java/com/library/controller/AuthController.java
+++ server/src/main/java/com/library/controller/AuthController.java
@@ -8,7 +8,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
